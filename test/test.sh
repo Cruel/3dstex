6567cf581a08c0f4e0bb2609b3377a05ed62685a
Thomas Edvalson <machin3@gmail.com>
2016-09-21 03:50:58 -0400

    Add test coverage for basic decoders too

--- test/test.sh
+++ test/test.sh
@@ -4,22 +4,19 @@ TARGET=./3dstex-test
 TEST_IMAGE=images/test.png
 TEST_OUTPUT=images/test.bin
 
+FORMATS=( rgba8 rgb8 rgba5551 rgb565 rgba4 la8 hilo8 l8 a8 la4 l4 a4 )
+
 set -x
 
 $TARGET -h > /dev/null
 
-$TARGET -o rgba8 $TEST_IMAGE rgba8.bin
-$TARGET -o rgb8 $TEST_IMAGE rgb8.bin
-$TARGET -o rgba5551 $TEST_IMAGE rgba5551.bin
-$TARGET -o rgb565 $TEST_IMAGE rgb565.bin
-$TARGET -o rgba4 $TEST_IMAGE rgba4.bin
-$TARGET -o la8 $TEST_IMAGE la8.bin
-$TARGET -o hilo8 $TEST_IMAGE hilo8.bin
-$TARGET -o l8 $TEST_IMAGE l8.bin
-$TARGET -o a8 $TEST_IMAGE a8.bin
-$TARGET -o la4 $TEST_IMAGE la4.bin
-$TARGET -o l4 $TEST_IMAGE l4.bin
-$TARGET -o a4 $TEST_IMAGE a4.bin
+for format in "${FORMATS[@]}"
+do
+	echo $format
+	$TARGET -o $format $TEST_IMAGE $format.bin
+	$TARGET -o png $format.bin $format.png
+done
+
 $TARGET -o etc1 -c1 $TEST_IMAGE etc1-c1.bin
 $TARGET -o etc1 -c2 $TEST_IMAGE etc1-c2.bin
 $TARGET -o etc1 -c3 $TEST_IMAGE etc1-c3.bin
