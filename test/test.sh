841fb31b2e9e1c65149757496f255b009b91a5fd
Thomas Edvalson <machin3@gmail.com>
2016-09-20 20:29:34 -0400

    Add test image and couple test cases

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 TARGET=./3dstex-test
+TEST_IMAGE=images/test.png
+TEST_OUTPUT=images/test.bin
 
 set -x
 
-$TARGET -h
+$TARGET -h > /dev/null
+
+$TARGET -o etc1 $TEST_IMAGE
+
+$TARGET -o rgba8 $TEST_IMAGE
+
+md5sum -c hash.md5
